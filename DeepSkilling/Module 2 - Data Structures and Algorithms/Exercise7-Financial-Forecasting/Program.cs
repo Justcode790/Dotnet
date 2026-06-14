f3f82762ff3c7389ad6c6abb9db34dfa7eb732fd
Just Code <justcode790@gmail.com>
2026-06-14 22:24:15 +0530

    exercise 2 and 7 of module 2

---
+++ DeepSkilling/Module 2 - Data Structures and Algorithms/Exercise7-Financial-Forecasting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static double PredictValue(double currentValue,double growthRate,int years){
+        if (years == 0)
+            return currentValue;
+
+        return PredictValue(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+
+    static void Main()
+    {
+        double currentValue = 10000;
+        double growthRate = 0.10;
+        int years = 3;
+
+        double futureValue =
+            PredictValue(currentValue, growthRate, years);
+        Console.WriteLine("Future Value = " + futureValue);
+    }
+}
